2bda50a3e5b62fabeddb165e6a58ce9095fe911a
Sanjay Chaudhary <55749386+sanjayTharu@users.noreply.github.com>
2023-02-14 15:00:58 +0545

    recommendation system

---
+++ movie recommendations system-Copy1.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[2]:
+
+
+movies=pd.read_csv(r'C:\Users\Tharu\OneDrive\Desktop\project\\movies.csv')
+movies.head()
+
+
+# In[3]:
+
+
+ratings=pd.read_csv(r'C:\Users\Tharu\OneDrive\Desktop\project\\ratings.csv',usecols=['userId','movieId','rating'])
+ratings.head()
+
+
+# In[4]:
+
+
+ratings.shape
+
+
+# In[5]:
+
+
+movies.shape
+
+
+# In[6]:
+
+
+movies_users=ratings.pivot(index='movieId',columns='userId',values='rating').fillna(0)
+movies_users.head()
+
+
+# In[7]:
+
+
+from scipy.sparse import csr_matrix
+
+
+# In[8]:
+
+
+mat_movies=csr_matrix(movies_users.values)
+
+
+# In[9]:
+
+
+from sklearn.neighbors import NearestNeighbors
+
+
+# In[10]:
+
+
+model=NearestNeighbors(metric='cosine',algorithm='brute',n_neighbors=20)
+model.fit(mat_movies)
+
+
+# In[12]:
+
+
+from fuzzywuzzy import process
+
+
+# In[15]:
+
+
+def recommender(movie_name,data,n):
+    idx=process.extractOne(movie_name,movies['title'])[2]
+    print('Movie Selected :',movies['title'][idx],'Index:',idx)
+    print('Searching for recommendation.................')
+    distance,indices=model.kneighbors(data[idx],n_neighbors=n)
+    for i in indices:
+        print(movies['title'][i].where(i!=idx))
+
+
+# In[18]:
+
+
+recommender('forrest gump',mat_movies,20)
+
+
+# In[ ]:
+
+
+
+
